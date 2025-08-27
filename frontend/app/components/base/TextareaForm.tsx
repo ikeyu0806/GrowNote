a58b90dee70987bfbea4fba6770e417fceca7ea1
ikeyu0806 <ikeyu0806@gmail.com>
2025-08-27 17:34:19 +0900

    implement TextareaForm

---
+++ frontend/app/components/base/TextareaForm.tsx
@@ -0,0 +1,30 @@
+export const TextareaForm = ({
+  id,
+  label,
+  value = '',
+  placeholder = '',
+  required = false,
+  rows = 4,
+}) => {
+  return (
+    <div className='mb-5'>
+      <label
+        htmlFor={id}
+        className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+      >
+        {label}
+      </label>
+      <textarea
+        id={id}
+        rows={rows}
+        value={value}
+        placeholder={placeholder}
+        required={required}
+        className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border 
+                   border-gray-300 focus:ring-blue-500 focus:border-blue-500 
+                   dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
+                   dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+      />
+    </div>
+  )
+}
--- frontend/app/routes/goals.new.tsx
+++ frontend/app/routes/goals.new.tsx
@@ -1,10 +1,11 @@
-import { InputForm } from "../components/base/InputForm"
+import { InputForm } from '../components/base/InputForm'
+import { TextareaForm } from '../components/base/TextareaForm'
 
 export default function CreateGoal() {
-  return <>
-    <InputForm
-        id='goalName'
-        label='目標タイトル'
-    />
-  </>
+  return (
+    <>
+      <InputForm id='goalTitle' label='目標タイトル' />
+      <TextareaForm id='goalDescription' label='説明' rows={8} />
+    </>
+  )
 }
