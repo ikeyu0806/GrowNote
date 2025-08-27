ed6e6cb7672b7f05b65eebfc68fcabe53dd75069
ikeyu0806 <ikeyu0806@gmail.com>
2025-08-27 14:34:03 +0900

    run prettier

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
-import flowbiteReact from "flowbite-react/plugin/vite";
+import flowbiteReact from 'flowbite-react/plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
