275d256fa4b16ba84a5351fc6ad7c9ecdd9f79a8
ikeyu0806 <ikeyu0806@gmail.com>
2025-08-27 17:04:54 +0900

    change Header link

--- frontend/app/components/layout/Header.tsx
+++ frontend/app/components/layout/Header.tsx
@@ -5,6 +5,27 @@ export const Header = () => {
         <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
           GrowNote
         </span>
+        <div className='hidden w-full md:block md:w-auto' id='navbar-default'>
+          <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+            <li>
+              <a
+                href='/'
+                className='block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                aria-current='page'
+              >
+                目標一覧
+              </a>
+            </li>
+            <li>
+              <a
+                href='/goals/new'
+                className='block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+              >
+                目標設定
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
