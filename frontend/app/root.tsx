bd6a298b498e3333c7839c10b58ae7165b397d78
ikeyu0806 <ikeyu0806@gmail.com>
2025-08-27 14:27:09 +0900

    install tailwind

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,15 +1,20 @@
 import { Links, Meta, Outlet, Scripts } from '@remix-run/react'
+import { Button } from "flowbite-react"
 
 export default function App() {
   return (
     <html>
       <head>
         <link rel='icon' href='data:image/x-icon;base64,AA' />
+        <link href="/app/tailwind.css" rel="stylesheet"></link>
         <Meta />
         <Links />
       </head>
       <body>
-        <h1>Hello TypeScript!</h1>
+         <h1 class="text-3xl font-bold underline">
+          Hello TypeScript!
+        </h1>
+        <Button>Click me</Button>
         <Outlet />
 
         <Scripts />
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
+import flowbiteReact from "flowbite-react/plugin/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [remix(), flowbiteReact(), tailwindcss()],
 })
