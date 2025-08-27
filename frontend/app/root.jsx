e88f366bff8ae439e1a9eb71584e4733a118269e
ikeyu0806 <ikeyu0806@gmail.com>
2025-08-27 13:16:00 +0900

    setup prettier

--- frontend/app/root.jsx
+++ frontend/app/root.jsx
@@ -1,18 +1,10 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts } from '@remix-run/react'
 
 export default function App() {
   return (
     <html>
       <head>
-        <link
-          rel="icon"
-          href="data:image/x-icon;base64,AA"
-        />
+        <link rel='icon' href='data:image/x-icon;base64,AA' />
         <Meta />
         <Links />
       </head>
@@ -23,5 +15,5 @@ export default function App() {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
