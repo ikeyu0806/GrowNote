de7d137a110128371e9efc95013e46911f898739
ikeyu0806 <ikeyu0806@gmail.com>
2025-08-31 18:13:48 +0900

    fix edit progressLog component

--- frontend/app/components/progressLog/EditForm.tsx
+++ frontend/app/components/progressLog/EditForm.tsx
@@ -3,15 +3,17 @@ import { useAtom } from 'jotai'
 import { progressLogProgressRateAtom } from '../../atoms/progressLogAtmos'
 
 export default function EditProgressLogForm() {
-  const [progressLogProgressRateAtom, setProgressLogProgressRateAtom] =
-    useAtom(goalTitleAtom)
+  const [progressLogProgressRate, setProgressLogProgressRate] = useAtom(
+    progressLogProgressRateAtom,
+  )
+
   return (
     <>
       <InputForm
         id='progressLogProgressRate'
         label='学習時間'
         value={progressLogProgressRateAtom}
-        onChange={(e) => setProgressLogProgressRateAtom(e.target.value)}
+        onChange={(e) => setProgressLogProgressRate(e.target.value)}
         type='number'
       />
     </>
