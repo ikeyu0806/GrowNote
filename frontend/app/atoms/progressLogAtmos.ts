ec0afcd4b80ff79090293d92c3e83ff15332ce5f
ikeyu0806 <ikeyu0806@gmail.com>
2025-08-31 15:17:36 +0900

    implement progressLogAtmos

---
+++ frontend/app/atoms/progressLogAtmos.ts
@@ -0,0 +1,14 @@
+import { atom } from 'jotai'
+
+export enum Mood {
+  Good = 'good',
+  Normal = 'normal',
+  Bad = 'bad',
+}
+
+export const progressLogDateAtom = atom('') // ISO文字列で管理
+export const progressLogContentAtom = atom('')
+export const progressLogStudyTimeAtom = atom<number | ''>('')
+export const progressLogProgressRateAtom = atom<number | ''>('')
+export const progressLogMoodAtom = atom<Mood | ''>('')
+export const progressLogGoalIdAtom = atom<number | null>(null)
