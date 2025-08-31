ba323057acf6cd12b32024d5306907ac6e9ad379
ikeyu0806 <ikeyu0806@gmail.com>
2025-08-31 18:25:29 +0900

    add content to progressLog/EditForm

--- frontend/app/components/progressLog/EditForm.tsx
+++ frontend/app/components/progressLog/EditForm.tsx
@@ -1,18 +1,32 @@
 import { InputForm } from '../base/InputForm'
+import { TextareaForm } from '../base/TextareaForm'
 import { useAtom } from 'jotai'
-import { progressLogProgressRateAtom } from '../../atoms/progressLogAtmos'
+import {
+  progressLogContentAtom,
+  progressLogProgressRateAtom,
+} from '../../atoms/progressLogAtmos'
 
 export default function EditProgressLogForm() {
+  const [progressLogContent, setProgressLogContent] = useAtom(
+    progressLogContentAtom,
+  )
   const [progressLogProgressRate, setProgressLogProgressRate] = useAtom(
     progressLogProgressRateAtom,
   )
 
   return (
     <>
+      <TextareaForm
+        id='goalDescription'
+        label='説明'
+        rows={8}
+        value={progressLogContent}
+        onChange={(e) => setProgressLogContent(e.target.value)}
+      />
       <InputForm
         id='progressLogProgressRate'
         label='学習時間'
-        value={progressLogProgressRateAtom}
+        value={progressLogProgressRate}
         onChange={(e) => setProgressLogProgressRate(Number(e.target.value))}
         type='number'
       />
